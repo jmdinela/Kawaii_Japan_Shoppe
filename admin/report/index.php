5d17882301ca4e50467469b784b26a37d03eed27
Moren <dinelajohnmoren@gmail.com>
2024-02-11 03:10:14 +0800

    Update index.php

--- admin/report/index.php
+++ admin/report/index.php
@@ -1,13 +1,13 @@
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
-<?php 
+<?php if ($_settings->chk_flashdata('success')) : ?>
+	<script>
+		alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+	</script>
+<?php endif; ?>
+<?php
 $from = isset($_GET['from']) ? $_GET['from'] : date("Y-m-d");
 $to = isset($_GET['to']) ? $_GET['to'] : date("Y-m-d");
 $type = isset($_GET['type']) ? $_GET['type'] : "all";
-$type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
+$type_arr = ['all' => "All", "1" => "Sales Invoices", "2" => "Services Invoices"];
 ?>
 <div class="card card-outline rounded-0 card-maroon">
 	<div class="card-body">
@@ -20,14 +20,14 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 							<input type="date" id="from" name="from" value="<?= $from ?>" class="form-control form-control-sm rounded-0">
 						</div>
 					</div>
-					
+
 					<div class="col-lg-3 col-md-4 col-sm-12">
 						<div class="form-group">
 							<label for="to" class="control-label">To</label>
 							<input type="date" id="to" name="to" value="<?= $to ?>" class="form-control form-control-sm rounded-0">
 						</div>
 					</div>
-					
+
 					<div class="col-lg-3 col-md-4 col-sm-12">
 						<div class="form-group">
 							<label for="type" class="control-label">Type</label>
@@ -64,19 +64,19 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 			<hr>
 			<table width="100%">
 				<tr>
-					<?php if($from == $to): ?>
-					<td width="33.33%">
-						<label for=""><b>Date:</b></label>
-						<span>&nbsp;<?= date("F j, Y", strtotime($from)) ?></span>
-					<?php else: ?>
-					<td width="33.33%">
-						<label for=""><b>From:</b></label>
-						<span>&nbsp;<?= date("F j, Y", strtotime($from)) ?></span>
-					</td>
-					<td width="33.33%">
-						<label for=""><b>To:</b></label>
-						<span>&nbsp;<?= date("F j, Y", strtotime($to)) ?></span>
-					</td>
+					<?php if ($from == $to) : ?>
+						<td width="33.33%">
+							<label for=""><b>Date:</b></label>
+							<span>&nbsp;<?= date("F j, Y", strtotime($from)) ?></span>
+						<?php else : ?>
+						<td width="33.33%">
+							<label for=""><b>From:</b></label>
+							<span>&nbsp;<?= date("F j, Y", strtotime($from)) ?></span>
+						</td>
+						<td width="33.33%">
+							<label for=""><b>To:</b></label>
+							<span>&nbsp;<?= date("F j, Y", strtotime($to)) ?></span>
+						</td>
 					<?php endif ?>
 					<td width="33.33%">
 						<label for=""><b>Type:</b></label>
@@ -86,13 +86,13 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 				</tr>
 			</table>
 			<br>
-			<?php 
+			<?php
 			$where = "";
-			if($from == $to)
+			if ($from == $to)
 				$where .= " date(`date_created`) = '{$from}' ";
 			else
 				$where .= " date(`date_created`) BETWEEN '{$from}' and '{$to}' ";
-			if(is_numeric($type) && $type > 0){
+			if (is_numeric($type) && $type > 0) {
 				$where .= " and `type` = '{$type}' ";
 			}
 			$invoices = $conn->query("SELECT * FROM `invoice_list` where {$where} order by abs(unix_timestamp(`date_created`)) asc");
@@ -115,20 +115,21 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 					</tr>
 				</thead>
 				<tbody>
-					<?php if($invoices->num_rows > 0): ?>
+					<?php $total = 0; ?>
+					<?php if ($invoices->num_rows > 0) : ?>
 						<?php $i = 1; ?>
-						<?php $total = 0; ?>
-						<?php while($row = $invoices->fetch_assoc()): ?>
-						<?php $total +=$row['total_amount']; ?>
+
+						<?php while ($row = $invoices->fetch_assoc()) : ?>
+							<?php $total += $row['total_amount']; ?>
 							<tr>
 								<th class="text-center"><?= ($i++) ?></th>
 								<td><?= $row['invoice_code'] ?></td>
 								<td><?= ucwords($row['customer_name']) ?></td>
 								<td><?= $type_arr[$row['type']] ?></td>
-								<td class='text-right'><?=number_format( $row['total_amount'] , 2) ?></td>
+								<td class='text-right'><?= number_format($row['total_amount'], 2) ?></td>
 							</tr>
 						<?php endwhile; ?>
-					<?php else: ?>
+					<?php else : ?>
 						<tr>
 							<th class="text-center" colspan="5">No records found!</th>
 						</tr>
@@ -137,7 +138,11 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 				<tfoot>
 					<tr>
 						<th class="text-center" colspan="4">Overall Total</th>
-						<th class="text-right"><?= number_format( $total , 2); ?></th>
+						<th class="text-right">
+							<?= number_format($total);
+							?>
+
+						</th>
 					</tr>
 				</tfoot>
 			</table>
@@ -146,28 +151,29 @@ $type_arr = ['all'=>"All", "1"=>"Sales Invoices", "2" => "Services Invoices" ];
 </div>
 <noscript id="style">
 	<style>
-		html, body{
-			min-height:unset !important;
-			margin:none;
+		html,
+		body {
+			min-height: unset !important;
+			margin: none;
 		}
 	</style>
 </noscript>
 <script>
-	$(document).ready(function(){
-		$('form#filterForm').submit(function(e){
+	$(document).ready(function() {
+		$('form#filterForm').submit(function(e) {
 			e.preventDefault()
 			start_loader()
 			var data = $(this).serialize();
-			location.href= "<?= base_url ?>admin/?page=report&"+data;
+			location.href = "<?= base_url ?>admin/?page=report&" + data;
 		})
-		$('#print').click(function(){
+		$('#print').click(function() {
 			var toPrint = $('#outPrint').clone()[0].outerHTML
 			var head = $('head').clone()
 			var customCSS = $($("noscript#style").html()).clone().outerHTML
 			var el = $("div")
 			head.append(customCSS)
 			start_loader()
-			var nw = window.open("","blank", "width="+$(window).width()+", height="+$(window).height()+",left=0, top=0")
+			var nw = window.open("", "blank", "width=" + $(window).width() + ", height=" + $(window).height() + ",left=0, top=0")
 			nw.document.querySelector("head").innerHTML = head[0].outerHTML
 			nw.document.querySelector("body").innerHTML = toPrint
 			nw.document.close()
